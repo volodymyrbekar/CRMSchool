5dea5206577eeee4a240cb3e27f50644a28a5a15
vova <vova.bekar7@gmail.com>
2024-10-11 14:08:27 +0300

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ python manage.py update_custom_id
 
 # Start the application using Gunicorn in production mode
 echo "Starting the application in production mode..."
-#gunicorn crmschool.wsgi:application --bind 0.0.0.0:8000
+gunicorn crmschool.wsgi:application --bind 0.0.0.0:8000
 
 #echo "WARNING: DEBUG is set to True! This is not recommended for production."
 #    # Start the application using Django's runserver
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
 
